321b46ead354f6da0eca12c9c8587b8d5dbbb646
murad0cs <65944713+murad0cs@users.noreply.github.com>
2020-09-21 16:43:50 +0600

    intern views

---
+++ views/intern_profile.php
@@ -0,0 +1,158 @@
+  
+<?php
+	require_once('../php/session_header.php');
+	require_once('../service/internservice.php');
+
+    
+?>
+
+
+<fieldset>
+			<h1 align="center">Online Doctor Management System</h1>
+            <table>
+                <legend>Account</legend>
+                <form>
+             
+                <tr>
+                  <td>
+                      <a href="profile.php"> View Profile</a>
+                  </td>
+                </tr> 
+                <tr>
+                    <td>
+                        <a href="changename.php"> Change Name</a>
+                    </td>
+                </tr>
+                    <tr>
+                    <td>
+                        <a href="picture.html">Upload Profile Picture</a>
+                    </td>
+                </tr>
+                    <tr>
+                    <td>
+                        <a href="change_password.html"> Change Password </a>
+                    </td>
+                </tr>
+                    <tr>
+                    <td>
+                        <a href="../php/logout.php"> Logout</a>
+                    </td>
+                </tr>
+                    <tr>
+                    <td>
+                        <a href="givs.php"> Give Suggestion </a>
+                    </td>
+                </tr>
+                    <tr>
+                    <td>
+                        <a href="eduqua.html"> Add Degree</a>
+                    </td>
+                </tr>
+                    <tr>
+                    <td>
+                        <a href="ratings.php">My Ratings</a>
+                    </td>
+                </tr>
+                    <tr>
+                    <td>
+                        <a href="about.php"> About Me</a>
+                    </td>
+                </tr>
+                    <tr>
+                    <td>
+                        <a href="firstaid.php"> First Aid </a>
+                    </td>
+                </tr>
+                    <tr>
+                    <td>
+                        <a href="searchdoc.php">Search Doctors</a>
+                    </td>
+                </tr>
+                    <tr>
+                    <td>
+                        <a href="searchhos.php">Search Hospitals</a>
+                    </td>
+                </tr>
+                    <tr>
+                    <td>
+                        <a href="intern_home.php">Home</a>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <a href="show_add_info.php"> Additional Info</a>
+                    </td>
+                </tr>
+                </form>
+            </table>
+    </fieldset>
+
+
+       
+      
+         
+<fieldset>
+    <legend><b>PROFILE</b></legend>
+    
+	<form>
+		<br/>
+		<table cellpadding="0" cellspacing="0">
+        
+			<tr>
+				<td>Name</td>
+				<td>:</td>
+                
+				<td>
+              <?php
+       
+              $a=  $_SESSION['userName'];
+      
+              $users = getByID($a);
+               echo $users['Name'];
+               ?>
+       
+                </td>
+				<td rowspan="7" align="center">
+                    <br/>
+                    
+				</td>
+			</tr>		
+			<tr><td colspan="3"><hr/></td></tr>
+			<tr>
+				<td>Email</td>
+				<td>:</td>
+				<td><?php
+                echo $users['Email'];
+                ?>
+
+     </td>
+			</tr>		
+			<tr><td colspan="3"><hr/></td></tr>			
+			<tr>
+				<td>Gender</td>
+				<td>:</td>
+				<td>
+                <?php
+                echo $users['Gender'];
+                ?>
+                </td>
+			</tr>
+			<tr><td colspan="3"><hr/></td></tr>
+			<tr>
+				<td>Date of Birth</td>
+				<td>: </td>
+				<td>
+                <?php
+                echo $users['DOB'];
+                ?>
+     </td>
+             </tr>   
+			
+          
+		</table>	
+        <hr/>
+       	
+	</form>
+    
+</fieldset>
