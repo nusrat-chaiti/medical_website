48276e44407f886f9caeed0d54f7d3f665b365c9
murad0cs <65944713+murad0cs@users.noreply.github.com>
2020-09-22 17:29:46 +0600

    Update check_changename.php

--- php/check_changename.php
+++ php/check_changename.php
@@ -3,14 +3,22 @@
   require_once('../service/internservice.php');
 
 	if(isset($_POST['submit'])){
+		
 
 		$name = $_POST['name'];
         $password = $_POST['password'];
         
         $uname =  $_SESSION['uname'];
-        
-
-	
+		if($name=="" && $password=="")
+		{
+			echo"Can not be empty";
+		}
+		else if(str_word_count($name)<2)
+		{
+			echo"Must have 2 words";
+		}
+		else
+		{
 			$user = [
 				'name'=> $name,
 				'password'=> $password,
@@ -28,6 +36,12 @@
 			}else{
 				header('location: ../views/changename.php?error=db_error');
 			}
+
+		}
+        
+
+	
+			
 		
 	}
 ?>
